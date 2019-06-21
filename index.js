28f24ee15ea452439141013adc851579ec8b04d3
Syl D <sdelescluse@atecna.fr>
2019-06-21 18:11:38 +0200

    feat (app) : Ignore sitemap.xml and display url add events

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const parser = require('xml2json');
 
 // create generator
 const generator = SitemapGenerator('https://www.test-site.fr', {
-    stripQuerystring: false,
+    stripQuerystring: true,
     filepath: './sitemap.xml',
     maxDepth: 10,
-    priorityMap: [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0]
+    priorityMap: [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0],
+    ignore: url => {
+        // Prevent URLs from being added that contain `<pattern>`.
+        return /[^>]+sitemap.xml/g.test(url)
+    }
 });
 
 // register event listeners
@@ -24,6 +28,11 @@ generator.on('done', () => {
         let json = JSON.parse(parser.toJson(data, {reversible: true}));
         console.log('urls:', json["urlset"]["url"].length);
     });
+});
+
+generator.on('add', (url) => {
+    // log url
+    console.log('add url:', url);
 });
 
 generator.on('error', (error) => {
